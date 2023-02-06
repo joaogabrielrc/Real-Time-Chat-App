5d9713670e49756297d83c8f2366ccbd3c12074b
João Gabriel R Cardoso <joaogabriel@cnnct.black>
2023-02-06 11:15:42 -0300

    Initial setup files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+/* eslint-disable no-console */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const hostname = process.env.HOSTNAME ?? '127.0.0.1';
+  const port = process.env.PORT ?? 8000;
+
+  await app.listen(port, hostname, () =>
+    console.log(`Server running at http://${hostname}:${port}/`)
+  );
 }
 bootstrap();
